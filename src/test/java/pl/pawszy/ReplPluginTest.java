e8a233393dbb36f868818e42f58f8e66edf3ba27
pwszpl <szppszpsz@gmail.com>
2022-08-23 18:19:38 +0200

    returning to previus version of test

--- src/test/java/pl/pawszy/ReplPluginTest.java
+++ src/test/java/pl/pawszy/ReplPluginTest.java
@@ -18,15 +18,15 @@ public class ReplPluginTest {
 
     @Test
     public void flattenFiles() throws IOException {
-        repl.setSrcDir("target/test-classes");
+        repl.setSrcDir("./target/test-classes");
         repl.execute();
 
-        File file1 = new File("src/test/resources/.flattened-pom.xml");
-        File file2 = new File("target/test-classes/pom.xml");
+        File file1 = new File("./src/test/resources/.flattened-pom.xml");
+        File file2 = new File("./target/test-classes/pom.xml");
         Assert.assertTrue(FileUtils.contentEquals(file1, file2));
 
-        File file3 = new File("src/test/resources/hierarchical/.flattened-pom.xml");
-        File file4 = new File("target/test-classes/hierarchical/pom.xml");
+        File file3 = new File("./src/test/resources/hierarchical/.flattened-pom.xml");
+        File file4 = new File("./target/test-classes/hierarchical/pom.xml");
         Assert.assertTrue(FileUtils.contentEquals(file3, file4));
     }
 
